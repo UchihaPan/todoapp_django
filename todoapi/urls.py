b7483cf1c17a42c79db5a266716c18f1f6892efc
panka <pankajshinde798@gmail.com>
2020-11-23 13:33:10 +0530

    bug fixx

--- todoapi/urls.py
+++ todoapi/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .views import TodoListCompletedView, TodoListCreateView, TodoListUpdateView, TodoCompleteView,signup,login
 
 urlpatterns = [
-    path('completed/', TodoListCompletedView.as_view(), name='completedapiview'),
-    path('create/', TodoListCreateView.as_view(), name='createapiview'),
+    path('apicompleted/', TodoListCompletedView.as_view(), name='completedapiview'),
+    path('apicreate/', TodoListCreateView.as_view(), name='createapiview'),
     path('update/<int:pk>/', TodoListUpdateView.as_view(), name='updateapiview'),
     path('<int:pk>/complete', TodoCompleteView.as_view(), name='completetodoapiapiview'),
     path('apisignup/', signup, name='apisignup'),
